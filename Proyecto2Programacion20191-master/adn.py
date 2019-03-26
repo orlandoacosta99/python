5b54b617eddd8af45d72ee5a292e6b248de75d46
orlandoacosta99 <36582050+orlandoacosta99@users.noreply.github.com>
2019-03-26 17:23:00 -0500

    adn

--- Proyecto2Programacion20191-master/adn.py
+++ Proyecto2Programacion20191-master/adn.py
@@ -1,50 +1,118 @@
 def obtener_complemento(base):
     # retorna caracter
-    pass
+    """
+    La función obtener complento que retorna el complemento de una base
+    str->str
+    >>> obtener_complemento('Adenina')
+    'A'
+
+    :param base: recibe un complemento base
+    :return: retorna una base complementada
+    """
 
 
 def generar_cadena_complementaria(adn):
-    pass
+   """
+   La función para generar la cadena complementaria
+   >>> generar_cadena_complementaria(A,T)
+
+   :param adn:
+   :return:
+   """
 
 
 def calcular_correspondencia(adn1, adn2):
     # retorna num
-    pass
+    """
+    La función para calcular el porcentaje de correspondencia de una cadena y otra
+
+    :param adn1:
+    :param adn2:
+    :return:
+    """
 
 
 def corresponden(adn1, adn2):
     # retorna Bool
-    pass
+    """
+    La función que nos valida la correspondencia entre dos cadenas
+
+    :param adn1:
+    :param adn2:
+    :return:
+    """
+
 
 
 def es_cadena_valida(adn):
-    pass
+   """
+   La función para validar si todos las bases son validas en la cadena
+
+   :param adn:
+   :return:
+   """
 
 
 def es_base(caracter):
-    pass
+    """
+    La función es base
+
+    :param caracter:
+    :return:
+    """
 
 
 def es_subcadena(adn1, adn2):
-    pass
+    """
+    Una función es sub cadena, que recibe dos cadenas de adn, las valida y verifica si una es una subcdena de la otra
+
+    :param adn1:
+    :param adn2:
+    :return:
+    """
 
 
 def reparar_dano(adn, base):
-    pass
+    """
+    Una función reparar daño que reemplaza los elementos no correspondientes con una base dada
+
+    :param adn:
+    :param base:
+    :return:
+    """
 
 
 def obtener_secciones(adn, n):
-    pass
+    """
+    Una función obtener secciones que entregue una lista de secciones, dada una cadena de adn y un numero de secciones
+    :param adn:
+    :param n:
+    :return:
+    """
 
 
 def obtener_complementos(lista_adn):
-    pass
+    """
+    Una función complementos_por_secciones que entregue una lista de cadenas de adn complementarias dada una lista de cadenas de adn
+
+    :param lista_adn:
+    :return:
+    """
 
 
 def unir_cadena(lista_adn):
-    pass
+    """
+    Una función unir cadenas que retorne una cadena de adn dada una lista de cadenas (debe validar que sean secuencias validas)
+
+    :param lista_adn:
+    :return:
+    """
 
 
 def complementar_cadenas(lista_adn):
-    pass
+    """
+    Una función complemento de cadenas que retorne una cadena complementaria dada una lista de cadenas (debe validar las secuencias)
 
+    :param lista_adn:
+    :return:
+    """
