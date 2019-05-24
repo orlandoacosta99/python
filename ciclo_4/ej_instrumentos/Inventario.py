bebc087da45c79dc596579cc45d1269c307d56b8
Orlando Camilo Acosta <oacosta00309@universidadean.edu.co>
2019-05-24 00:01:55 -0500

    ins

--- ciclo_4/ej_instrumentos/Inventario.py
+++ ciclo_4/ej_instrumentos/Inventario.py
@@ -1,61 +1,55 @@
 class Instrumentos:
 
     nombre_instrumento = ''
-    melodia_armonica = ''
-    afinacion = ''
     tipo_instrumento = ''
     n_instrumentos = 0
 
-    def __init__(self, nombre_instrumento, afinacion, melodia_armonica, tipo_instrumento):
+    def __init__(self, nombre_instrumento, tipo_instrumento, n_instrumentos):
         self.nombre_instrumento = nombre_instrumento
-        self.afinacion = afinacion
-        self.melodia_armonica = melodia_armonica
         self.tipo_instrumento = tipo_instrumento
-
-    def cantidad(self, n_instrumentos):
-
         self.n_instrumentos = n_instrumentos
 
-        if n_instrumentos < 0:
-            raise ValueError('el numero de instrumentos es negativo')
+    def clas_instrumento(self):
 
-        return self.n_instrumentos
+        if (self.tipo_instrumento == 'cuerdas'):
+            return f'el instrumento {self.nombre_instrumento} es de {self.tipo_instrumento}'
 
-    def tipo_instrumento(self, cuerdas, percusion):
+        elif (self.tipo_instrumento == 'percusion'):
 
-        if (cuerdas == 'cuerdas' and cuerdas != percusion):
-
-            tipo_ins = cuerdas()
-
-        elif (percusion == 'persucion' and percusion != cuerdas):
-
-            tipo_ins = percusion()
+            return f'el instrumento {self.nombre_instrumento} es de {self.tipo_instrumento}'
         else:
 
-            raise ValueError('No se encuentra instrumento')
+            raise ValueError('No se encuentra el tipo de instrumento')
 
-        return tipo_ins
+    def cantidad(self):
+        if self.n_instrumentos < 0:
+            raise ValueError('el numero de instrumentos es negativo')
+
+        return f'el instrumento {self.nombre_instrumento} es de {self.tipo_instrumento} y tinene la cantidad de {self.n_instrumentos} instrumentos'
+
+class Cuerdas(Instrumentos):
+    n_cuerdas = 0
+    esc_afinacion = ''
 
     def cuerdas(self, n_cuerdas, esc_afinacion):
+        #super(Cuerdas, self).__init__(nombre_instrumento, tipo_instrumento, n_instrumentos, n_cuerdas, esc_afinacion)
         self.n_cuerdas = n_cuerdas
         self.esc_afinacion = esc_afinacion
 
-        # esc_afinacion = []
-
-        if n_cuerdas < 0:
+        if self.n_cuerdas < 0:
             raise ValueError(f'el {self.nombre_instrumento} no puede tener cuerdas negativas')
-        # else:
-        #     for x in range(1, len(n_cuerdas)):
-        #         nom = str(input('ingrese nombre de la cuerda'))
-        #         esc_afinacion.append(nom)
+        return f'el instrumento {self.nombre_instrumento}  es de {self.tipo_instrumento}, tiene {self.n_cuerdas} cuerdas y estan afinadas en {self.esc_afinacion} y tiene  la cantidad de {self.n_instrumentos} instrumentos'
 
-        return f'el instrumento {self.nombre_instrumento} tiene {n_cuerdas} cuerdas y estan afinadas en {esc_afinacion}'
+class Percusion(Instrumentos):
+    diametro = 0
+    frecuuencia = 0
 
     def percusion(self, diametro, frecuencia):
+
         self.diametro = diametro
         self.frecuencia = frecuencia
 
         if diametro < 0 or frecuencia < 0:
             raise ValueError('No hay diametros ni frecuencias negativas')
 
-        return f'el instrumento {self.nombre_instrumento} tiene un diametro de {diametro} cm y una frecuencia de vibracion es de {frecuencia} hz'
+        return f'el instrumento {self.nombre_instrumento} es de {self.tipo_instrumento},  tiene un diametro de {diametro} cm y una frecuencia de vibracion es de {frecuencia} hz y tiene  la cantidad de {self.n_instrumentos} instrumentos'
--- ciclo_4/ej_instrumentos/test_instrumentos.py
+++ ciclo_4/ej_instrumentos/test_instrumentos.py
@@ -1,44 +1,58 @@
 from unittest import TestCase
 from Inventario import Instrumentos
+from Inventario import Cuerdas
+from Inventario import Percusion
 
 class TestInstrumentos(TestCase):
-    def test_cantidad(self):
-        dado = Instrumentos('Violonchelo', 'La', 'jazz', 'cuerdas')
-        espero = 4
-        recibo = dado.cantidad(4)
+    def test_clas_instrumento(self):
+        dado = Instrumentos('Violonchelo', 'cuerdas', 4)
+        espero = 'el instrumento Violonchelo es de cuerdas'
+        recibo = dado.clas_instrumento()
         self.assertEqual(espero, recibo)
 
-        self.assertRaises(ValueError, dado.cantidad, -2)
 
-    # def test_tipo_instrumento(self):
-    #     dado = Instrumentos('guitarra', 'RE', 'rock')
-    #     espero = ''
-    #     recibo = dado.tipo_instrumento('cuerdas')
-    #     self.assertEqual(espero, recibo)
-    #
-    #     dado = Instrumentos('caja', 'no tiene', 'vallenato')
-    #     espero = ''
-    #     recibo = dado.tipo_instrumento('percucion')
-    #     self.assertEqual(espero, recibo)
-    #
-    #     dado = Instrumentos('cordeon', ' SOL', 'vallenato', ' ')
-    #     self.assertRaises(ValueError, dado.tipo_instrumento, ' ')
+        dado = Instrumentos('bombo', 'percusion', 5)
+        espero = 'el instrumento bombo es de percusion'
+        recibo = dado.clas_instrumento()
+        self.assertEqual(espero, recibo)
 
+        dado = Instrumentos('trompeta', 'aire', 5)
+        self.assertRaises(ValueError, dado.clas_instrumento)
+
+    def test_cantidad(self):
+
+        dado = Instrumentos('guitarra', 'cuerdas', 10)
+        espero = 'el instrumento guitarra es de cuerdas y tinene la cantidad de 10 instrumentos'
+        recibo = dado.cantidad()
+        self.assertEqual(espero, recibo)
+
+        dado = Instrumentos('caja', 'percusion', 7)
+        espero = 'el instrumento caja es de percusion y tinene la cantidad de 7 instrumentos'
+        recibo = dado.cantidad()
+        self.assertEqual(espero, recibo)
+
+        dado = Instrumentos('ukelele', 'cuerdas', -82)
+        self.assertRaises(ValueError, dado.cantidad)
+
+class TestCuerdas(TestCase):
     def test_cuerdas(self):
-        dado = Instrumentos('Violin', 'MI', 'Jazz', 'cuerdas')
-        espero = 'el instrumento Violin tiene 4 cuerdas y estan afinadas en MI,LA,RE,SOL'
+        dado = Cuerdas('Violin', 'cuerdas', 12)
+        espero = 'el instrumento Violin  es de cuerdas, tiene 4 cuerdas y estan afinadas en MI,LA,RE,SOL y tiene  la cantidad de 12 instrumentos'
         resultado = dado.cuerdas(4, 'MI,LA,RE,SOL')
         self.assertEqual(espero, resultado)
 
-        dado = Instrumentos('Bajo', 'MI', 'ROCK', 'cuerdas')
+        dado = Cuerdas('Bajo', 'cuerdas', '12')
+        espero = 'el Bajo no puede tener cuerdas negativas'
+        resultado= dado.cuerdas
         self.assertRaises(ValueError, dado.cuerdas, -4, 'Mi, La, Re, Sol')
 
+class TestPercusion(TestCase):
 
     def test_percusion(self):
-        dado = Instrumentos('Tambor', 'no tiene', 'jazz', 'percucion')
-        espero = 'el instrumento Tambor tiene un diametro de 25 cm y una frecuencia de vibracion es de 40 hz'
+        dado = Percusion('Tambor', 'percucion', 8)
+        espero = 'el instrumento Tambor es de percucion,  tiene un diametro de 25 cm y una frecuencia de vibracion es de 40 hz y tiene  la cantidad de 8 instrumentos'
         resultado = dado.percusion(25, 40)
         self.assertEqual(espero, resultado)
 
-        dado = Instrumentos('Tambora', 'no tiene', 'merengue', 'percucion')
+        dado = Percusion('Tambora', 'percucion', 4)
         self.assertRaises(ValueError, dado.percusion, -11, 271)
